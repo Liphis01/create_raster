1cad3bb84cf12d1d149eea4a743a3f3260892b13
gillarlo <louis.gillard@ensta-bretagne.org>
2022-11-30 22:17:04 +0100

    HSL to RGB conversion added

--- src/HSL.cpp
+++ src/HSL.cpp
@@ -56,3 +56,47 @@ static void RGBToHSL(int R, int G, int B, int &H, int &S, float &L)
 
 	return;
 }
+
+static float HueToRGB(float v1, float v2, float vH) {
+	if (vH < 0)
+		vH += 1;
+
+	if (vH > 1)
+		vH -= 1;
+
+	if ((6 * vH) < 1)
+		return (v1 + (v2 - v1) * 6 * vH);
+
+	if ((2 * vH) < 1)
+		return v2;
+
+	if ((3 * vH) < 2)
+		return (v1 + (v2 - v1) * ((2.0f / 3) - vH) * 6);
+
+	return v1;
+}
+
+static void HSLToRGB(int H, int S, float L, int &R, int &G, int &B) {
+	unsigned char r = 0;
+	unsigned char g = 0;
+	unsigned char b = 0;
+
+	if (S == 0)
+	{
+		r = g = b = (unsigned char)(L * 255);
+	}
+	else
+	{
+		float v1, v2;
+		float hue = (float)H / 360;
+
+		v2 = (L < 0.5) ? (L * (1 + S)) : ((L + S) - (L * S));
+		v1 = 2 * L - v2;
+
+		r = (unsigned char)(255 * HueToRGB(v1, v2, hue + (1.0f / 3)));
+		g = (unsigned char)(255 * HueToRGB(v1, v2, hue));
+		b = (unsigned char)(255 * HueToRGB(v1, v2, hue - (1.0f / 3)));
+	}
+
+	return;
+}
